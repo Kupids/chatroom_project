401bfe0a114e2be591a8419e66e678438bd4440a
Kupids <141076370+Kupids@users.noreply.github.com>
2026-05-16 11:00:06 +0300

    im still lost in the sauce

--- fastapi_stuff.py
+++ fastapi_stuff.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI , WebSocket , WebSocketDissconect
+from fastapi import Depends , WebSocket , WebSocketDissconect
 from sqlalchemy.ext.asyncio import AsyncSession , create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_async_engine("postgresql+asyncpg:://matan:matan123@localhost:5432/backend_stuff")
-async_session - sessionmaker(engine , class_=AsyncSession , expire_on_commit=False)
+async_session = sessionmaker(engine , class_=AsyncSession , expire_on_commit=False)
 
 #here we create the dependency for the websocket request to yeild a new db
 # connection everytime
@@ -31,9 +31,19 @@ class ConnectionManager:
 
 #giving a var to the class
 manager = ConnectionManager
-app = FastAPI()
 
 @app.websocket("/ws")
-async def1
+async def websocket_server(websocket : WebSocket , db : AsyncSession = Depends(generate_db)) -> None:
+    manager.connect(websocket)
+    await websocket.accept()
+    try:
+        while True:
+            msg = await websocket.receive_text() 
+            with AsyncSession as session:
+                statement = insert(messages).values(username=(idfkyetbro),Messages=msg)
+                result = await session.execute(statement)
+            websocket.broadcast(msg)
+        except WebSocketDisconnect:
+            print('someone dced')
 
 
